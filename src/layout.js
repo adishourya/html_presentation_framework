00eff91cd90c4c7e2484f04c658caba172bc9a82
aditya shourya <adi@adityas-MacBook-Air.local>
2025-12-17 16:52:50 +0100

    added headers and footers and a nice lift on overview

--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,33 @@
 const slides = Array.from(document.querySelectorAll('.slide'));
+const totalSlides = slides.length -1;
+
+// headers and footers
+// slide numbering
+slides.forEach((slide, i) => {
+  slide.dataset.index = i ; // 1-based numbering
+  const footer = document.createElement('div');
+  footer.className = 'slide-footer-right';
+  footer.textContent = `${i} / ${totalSlides}`;
+  slide.appendChild(footer);
+});
+
+// footer left
+slides.forEach((slide, i) => {
+  const footer = document.createElement('div');
+  footer.className = 'slide-footer-left';
+  footer.textContent = "Maastricht University";
+  slide.appendChild(footer);
+});
+
+// header right
+slides.forEach((slide, i) => {
+  const footer = document.createElement('div');
+  footer.className = 'slide-header-right';
+  footer.textContent = "Topic";
+  slide.appendChild(footer);
+});
+
+
 let current = 0;
 
 /* --- URL sync --- */
@@ -17,18 +46,38 @@ function readURL() {
 }
 
 /* --- Slide handling --- */
+
 function showSlide(index) {
   slides.forEach((s, i) => {
-    s.classList.remove('active', 'prev');
-    if (i === index) s.classList.add('active');
-    if (i === index - 1) s.classList.add('prev');
+    s.classList.remove('active', 'prev', 'animate');
+
+    if (i === index) {
+      s.classList.add('active');
+      s.offsetHeight; // force reflow
+      s.classList.add('animate');
+    }
+
+    if (i === index - 1) {
+      s.classList.add('prev');
+    }
+
+    const footer = s.querySelector('.slide-footer');
+    if (footer) {
+      footer.textContent = `${i + 1} / ${slides.length}`;
+    }
   });
+
   updateURL();
 }
+
 
 function next() {
   if (current < slides.length - 1) {
     current++;
+    showSlide(current);
+  }
+  else if (current == slides.length -1){
+    current = 0;
     showSlide(current);
   }
 }
@@ -51,6 +100,7 @@ slides.forEach((slide, i) => {
   });
 });
 
+
 /* --- Fullscreen --- */
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
@@ -65,13 +115,18 @@ function toggleOverview() {
   document.body.classList.toggle('overview');
 }
 
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
+
 /* --- Keyboard --- */
 document.addEventListener('keydown', e => {
-  if (e.key === 'ArrowRight' || e.key === ' ') next();
-  if (e.key === 'ArrowLeft') prev();
+  if (e.key === 't') toggleTheme(); // toggle theme
+  if (e.key === 'ArrowRight' ||e.key=="ArrowDown" || e.key == "l" || e.key === 'j') next();
+  if (e.key === 'ArrowLeft' || e.key=="ArrowUp" || e.key == "h" || e.key == "k") prev();
   if (e.key === 'f') toggleFullscreen();
   if (e.key === '-') toggleOverview();
-  if (e.key === 'Escape') toggleOverview();
 });
 
 /* --- Init --- */
